481f8a161eafffbc4056897060cf5e441b7152f2
olivia pasion <oap9820@gmail.com>
2022-07-27 15:50:51 -0700

    test, route, and model buit for id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,21 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+const { pokemon } = require('../data/pokemon')
+
+describe('pokemon routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/pokemon:id should return pokemon detail', async () => {
+    const res = await request(app).get('/pokemon/1');
+    const Pikachu = {
+      id: '1',
+      name: 'Pikachu',
+      type: 'electric',
+      index: 025
+    };
+    expect(res.body).toEqual(Pikachu);
   });
   afterAll(() => {
     pool.end();
