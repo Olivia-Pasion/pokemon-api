c941e94a69e8f049826bbcf08fb9d48c424628b3
olivia pasion <oap9820@gmail.com>
2022-07-27 17:17:49 -0700

    / passes test and works

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,10 +3,18 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+
 describe('pokemon routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  it('/pokemon should return a list of pokemon', async () => {
+    const res = await request(app).get('/pokemon');
+    expect(res.body[0]).toEqual({ id: expect.any(String), name: expect.any(String) });
+  });
+
+
   it('/pokemon/:id should return pokemon detail', async () => {
     const res = await request(app).get('/pokemon/1');
     const Pikachu = {
